5d4edc51732b752f3682085a8ccd751863fcec9f
yuuki-katsuta <reptpr0314@gmail.com>
2020-09-24 22:00:48 +0900

    解答の正誤判機能追加定

--- js/main.js
+++ js/main.js
@@ -20,8 +20,7 @@
   //currentNum という変数名で、最初はインデックスが 0 のクイズから始めたいので 0 としてあげれば OK です。
   let currentNum = 0
 
-  //問題文をセット
-  question.textContent = quizSet[currentNum].q
+
 
   //選択肢のシャッフルを処理
   //shuffle() という関数を作って、引数に配列を渡したら、その配列をシャッフルして返してあげる、と書いていく
@@ -44,20 +43,43 @@
     return arr; //シャッフルされた配列を返す
   }
 
-  //選択肢のシャッフルを実行し、帰ってきた配列をshuffledChoicesに経代入
-  const shuffledChoices = shuffle([...quizSet[currentNum].c])
+  //判定の処理をしていく　
+  function checkaAnsewer(li) {
+    if (li.textContent === quizSet[currentNum].c[0]) {
+      //選択肢の０番目は正解にしといた
+      li.classList.add('correct')
 
-  //選択肢をセット（配列からループで値を取得する）
-  shuffledChoices.forEach(choice => {
-    //choiceにはシャッフルされた選択肢の配列が帰ってくる
-    const li = document.createElement('li')
-    li.textContent = choice
-    choices.appendChild(li)
-  })
+    } else {
+      li.classList.add('wrong')
+    }
+  }
 
-  console.log(quizSet[currentNum].c)
-  //配列やオブジェクトを引数にすると、値のコピーが関数に渡されるのではなくて参照が渡されるので、渡した引数を関数の中で書き換えてしまうと、引数にした大元の配列も書き換えられてしまう
-  //よって大本の配列も変更されてしまう
-  //そのため、スプレッド演算子を利用
 
+  //画面描画の処理は関数にまとめておきましょう。
+  function setQuiz() {
+    //問題文をセット
+    question.textContent = quizSet[currentNum].q
+
+    //選択肢のシャッフルを実行し、帰ってきた配列をshuffledChoicesに経代入
+    const shuffledChoices = shuffle([...quizSet[currentNum].c])
+
+    //選択肢をセット（配列からループで値を取得する）
+    shuffledChoices.forEach(choice => {
+      //choiceにはシャッフルされた選択肢の配列が帰ってくる
+      const li = document.createElement('li')
+      li.textContent = choice
+      //選択肢をクリックしたときの処理
+      li.addEventListener('click', () => {
+        checkaAnsewer(li)
+      })
+      choices.appendChild(li)
+    })
+
+    console.log(quizSet[currentNum].c)
+    //配列やオブジェクトを引数にすると、値のコピーが関数に渡されるのではなくて参照が渡されるので、渡した引数を関数の中で書き換えてしまうと、引数にした大元の配列も書き換えられてしまう
+    //よって大本の配列も変更されてしまう
+    //そのため、スプレッド演算子を利用
+  }
+
+  setQuiz()
 }
