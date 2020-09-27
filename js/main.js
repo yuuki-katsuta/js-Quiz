458de3ec329e8974d3596d97cf8110659487bfe1
yuuki-katsuta <reptpr0314@gmail.com>
2020-09-27 17:26:39 +0900

    score表示機能追加

--- js/main.js
+++ js/main.js
@@ -6,15 +6,18 @@
   const question = document.getElementById('question')
   const choices = document.getElementById('choices')
   const btn = document.getElementById('btn')
+  //結果を表示
+  const result = document.getElementById('result')
+  //scoreを表示する領域
+  const scoreLabel = document.querySelector('#result > p')
 
   // クイズのデータはオブジェクトの配列で持っておく
   //この中で正解が必ず最初の要素、つまりインデックスが 0 番目の要素になるようなルールにしておきましょう。
-  const quizSet = [
+  const quizSet = shuffle([ //shuffleを適用した
     { q: 'What is A?', c: ['A0', 'A1', 'A2'] },
     { q: 'What is B?', c: ['B0', 'B1', 'B2'] },
     { q: 'What is C?', c: ['C0', 'C1', 'C2'] },
-  ]
-
+  ])
 
   //今何問目のクイズを解いているかを変数で持っておきましょう。
   //currentNum という変数名で、最初はインデックスが 0 のクイズから始めたいので 0 としてあげれば OK です。
@@ -23,6 +26,9 @@
   //解答状況を管理する
   let isAnswered
 
+  //正答数を管理したいので score という変数を定義
+  let score = 0
+  //正解したときに＋＋とする
 
 
   //選択肢のシャッフルを処理
@@ -58,14 +64,13 @@
     if (li.textContent === quizSet[currentNum].c[0]) {
       //選択肢の０番目は正解にしといた
       li.classList.add('correct')
-
+      score++
     } else {
       li.classList.add('wrong')
     }
     //選択肢を選んだら、次に行くために、ボタンから disabled クラスを外して青いボタンになるようにしていきましょう。
     btn.classList.remove('disabled')
   }
-
 
   //画面描画の処理は関数にまとめておきましょう。
   function setQuiz() {
@@ -95,8 +100,8 @@
       choices.appendChild(li)
     })
 
-    //最後の問題になったとき、netxボタンの文字を変える
-    if (currentNum === setQuiz.length) {
+    //最後の問題になったとき、nextボタンの文字を変える
+    if (currentNum === quizSet.length - 1) {
       btn.textContent = 'スコアを見る'
     }
 
@@ -111,14 +116,21 @@
   //next ボタンを押したときの処理
   btn.addEventListener('click', () => {
     if (btn.classList.contains('disabled')) {
-      return;　//kこれでnext ボタン連打を阻止している
+      return;　//これでnext ボタン連打を阻止している
+      //returnでこれ以降の処理は停止する
     }
     //選択肢をクリックした時点でdisabledクラスは外される
     //nextボタンでdisabledクラスはつけられる
     btn.classList.add('disabled')//灰色のボタン
 
-    currentNum++
-    setQuiz()
+    //最後の問題を答えたとき
+    if (currentNum === quizSet.length - 1) {
+      scoreLabel.textContent = `${score}/${quizSet.length}`
+      result.classList.remove('hidden')
+    } else {
+      currentNum++
+      setQuiz()
+    }
     //Next ボタンを押すと…、次の問題が出たのですが、前の問題の選択肢が残ってしまっているので
     //setQuiz() で回答の選択肢を表示する前に、一度全部の選択肢を消してあげましょう。
     //どうするかというと、 choices の最初の子要素がある限り choices の最初の子要素を消す、というテクニックがよく使われます。
